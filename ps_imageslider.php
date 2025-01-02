0831f2c76be854f3264a4aec92d5a3befff25334
agentpietrucha <agentpietrucha@gmail.com>
2025-01-02 13:11:23 +0100

    Bump version and author

--- ps_imageslider.php
+++ ps_imageslider.php
@@ -52,8 +52,8 @@ class Ps_ImageSlider extends Module implements WidgetInterface
     {
         $this->name = 'ps_imageslider';
         $this->tab = 'front_office_features';
-        $this->version = '3.2.1';
-        $this->author = 'PrestaShop';
+        $this->version = '3.3.0';
+        $this->author = 'agentpietrucha';
         $this->need_instance = 0;
         $this->secure_key = Tools::hash($this->name);
         $this->bootstrap = true;
@@ -61,7 +61,7 @@ class Ps_ImageSlider extends Module implements WidgetInterface
         parent::__construct();
 
         $this->displayName = $this->trans('Image slider', [], 'Modules.Imageslider.Admin');
-        $this->description = $this->trans('Add sliding images to your homepage to welcome your visitors in a visual and friendly way.', [], 'Modules.Imageslider.Admin');
+        $this->description = $this->trans('Add sliding images and videos to your homepage to welcome your visitors in a visual and friendly way.', [], 'Modules.Imageslider.Admin');
         $this->ps_versions_compliancy = ['min' => '1.7.5.0', 'max' => _PS_VERSION_];
 
         $this->templateFile = 'module:ps_imageslider/views/templates/hook/slider.tpl';
