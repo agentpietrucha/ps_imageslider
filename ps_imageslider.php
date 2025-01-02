453cd85d99ae6b6524231a85550334c176d632c0
agentpietrucha <agentpietrucha@gmail.com>
2025-01-02 12:16:20 +0100

    Remove poster when uploading image file

--- ps_imageslider.php
+++ ps_imageslider.php
@@ -224,7 +224,19 @@ class Ps_ImageSlider extends Module implements WidgetInterface
               PRIMARY KEY (`id_homeslider_slides`)
             ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=UTF8;
         ');
-
+        var_dump('
+            CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'homeslider_slides_lang` (
+              `id_homeslider_slides` int(10) unsigned NOT NULL,
+              `id_lang` int(10) unsigned NOT NULL,
+              `title` varchar(255) NOT NULL,
+              `description` text NOT NULL,
+              `legend` varchar(255) NOT NULL,
+              `url` varchar(255) NOT NULL,
+              `image` varchar(255) NOT NULL,
+              `poster` varchar(255) NOT NULL,
+              PRIMARY KEY (`id_homeslider_slides`,`id_lang`)
+            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=UTF8;
+        ');
         /* Slides lang configuration */
         $res &= Db::getInstance()->execute('
             CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'homeslider_slides_lang` (
@@ -235,6 +247,7 @@ class Ps_ImageSlider extends Module implements WidgetInterface
               `legend` varchar(255) NOT NULL,
               `url` varchar(255) NOT NULL,
               `image` varchar(255) NOT NULL,
+              `poster` varchar(255) NOT NULL,
               PRIMARY KEY (`id_homeslider_slides`,`id_lang`)
             ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=UTF8;
         ');
@@ -550,6 +563,7 @@ class Ps_ImageSlider extends Module implements WidgetInterface
                             @unlink($temp_name);
                         }
                         $slide->image[$language['id_lang']] = $salt . '_' . $_FILES['image_' . $language['id_lang']]['name'];
+                        $slide->poster[$language['id_lang']] = null;
                     } else {
                         $errors[] = $this->trans('Unsupported file extension: %extension%', ['%extension%' => $file_extension], 'Modules.Imageslider.Admin');
                         continue;
